f6f4f683b839a1f20412c0953a45c311fbe54ff4
alessandro <alessandromic.censi@gmail.com>
2020-05-02 12:19:48 +0200

    "Course Menu UI completed"

--- app/src/main/java/com/example/ecoleenligne/LessonMenuActivity.java
+++ app/src/main/java/com/example/ecoleenligne/LessonMenuActivity.java
@@ -4,14 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.ecoleenligne.adapters.ChaptersAdapter;
 import com.example.ecoleenligne.models.UserInfo;
 
 import java.util.ArrayList;
 
-public class LessonMenuActivity extends AppCompatActivity {
+public class LessonMenuActivity extends AppCompatActivity implements View.OnClickListener {
     private TextView lessonMenuTitle;
 
     @Override
@@ -22,6 +24,11 @@ public class LessonMenuActivity extends AppCompatActivity {
         String lessonName = getIntent().getStringExtra("lessonName");
         UserInfo currentUser = getIntent().getParcelableExtra("user");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        findViewById(R.id.course_card_view_item).setOnClickListener(this);
+        findViewById(R.id.video_card_view_item).setOnClickListener(this);
+        findViewById(R.id.resume_card_view_item).setOnClickListener(this);
+        findViewById(R.id.quiz_card_view_item).setOnClickListener(this);
+        findViewById(R.id.exercise_card_view_item).setOnClickListener(this);
         lessonMenuTitle = findViewById(R.id.tv_lesson_menu_title);
         lessonMenuTitle.setText(lessonName + " lesson");
     }
@@ -37,4 +44,27 @@ public class LessonMenuActivity extends AppCompatActivity {
         return true;
     }
 
+    @Override
+    public void onClick(View v) {
+        String message = "";
+        switch (v.getId()){
+            case R.id.video_card_view_item:
+                message = "Video Item clicked!";
+                break;
+            case R.id.course_card_view_item:
+                message = "Course Item clicked!";
+                break;
+            case R.id.resume_card_view_item:
+                message = "Resume Item clicked!";
+                break;
+            case R.id.quiz_card_view_item:
+                message = "Quiz Item clicked!";
+                break;
+            case R.id.exercise_card_view_item:
+                message = "Exercise Item clicked!";
+                break;
+
+        }
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
 }
